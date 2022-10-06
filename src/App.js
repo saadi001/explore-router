23a98d1c248c9fd48441479dc9a2799ea2535252
Sheikh sadi <you@example.com>
2022-10-06 21:33:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './Components/About/About';
 import './App.css';
+import Home from './Components/Home/Home';
+import Products from './Components/Products/Products';
 
 function App() {
+  const router = createBrowserRouter([
+      { path:'/', element: <Home></Home>},
+      { path:'home', element: <Home></Home>},
+      {path: 'about', element: <About></About>},
+      { path: 'products', element: <Products></Products>}
+
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Products = () => {
+     return (
+          <div>
+               <Header></Header>
+               <h1>This is products page</h1>
+          </div>
+     );
+};
+
+export default Products;
